00f2e40dd4afda57057dbd23eba07faa94d168fe
yoloVoe <mainali.subrat@utexas.edu>
2019-10-12 16:14:35 -0500

    colorize logs; hide benign logs from gfx and winit

--- src/main.rs
+++ src/main.rs
@@ -42,15 +42,24 @@ mod game;
 fn setup_logger() -> Result<(), fern::InitError> {
     fern::Dispatch::new()
         .format(|out, message, record| {
+            let colors = fern::colors::ColoredLevelConfig::new()
+                .debug(fern::colors::Color::Magenta)
+                .trace(fern::colors::Color::Blue)
+                .info(fern::colors::Color::Green)
+                .warn(fern::colors::Color::Yellow)
+                .error(fern::colors::Color::Red);
+
             out.finish(format_args!(
                 "{}[{}][{}] {}",
                 chrono::Local::now().format("[%Y-%m-%d][%H:%M:%S]"),
                 record.target(),
-                record.level(),
+                colors.color(record.level()),
                 message
             ))
         })
         .level(log::LevelFilter::Debug)
+        .level_for("winit", log::LevelFilter::Warn)
+        .level_for("gfx_device_gl", log::LevelFilter::Warn)
         .chain(std::io::stdout())
         .chain(fern::log_file("output.log")?)
         .apply()?;
@@ -58,7 +67,7 @@ fn setup_logger() -> Result<(), fern::InitError> {
 }
 
 fn main() {
-    setup_logger();
+    setup_logger().unwrap();
 
     // Make a Context and an EventLoop.
     let (mut ctx, mut event_loop) =
