a624f59c2c4672c84461622aab28ae4bb1c17806
AlterionX <ben.xu.cs@gmail.com>
2019-10-28 15:34:28 -0500

    Make single element array not an array.

--- src/game/arena/platform.rs
+++ src/game/arena/platform.rs
@@ -9,7 +9,7 @@ use crate::{physics};
 pub struct Platform {
     mode: Option<BlendMode>,
     /// The portion occupied by the platform.
-    body: [physics::BoundingBox; 1],
+    body: physics::BoundingBox,
     /// If a player is allowed to move through the platform.
     can_move_through: bool,
     // TODO: Add storage for the assets' handles.
@@ -17,7 +17,7 @@ pub struct Platform {
 
 impl physics::Collidable for Platform {
     fn get_hitboxes<'tick>(&'tick self) -> &'tick[physics::BoundingBox] {
-        &self.body
+        std::slice::from_ref(&self.body)
     }
     fn get_effects(&self) {}
 }
@@ -26,11 +26,12 @@ impl Drawable for Platform {
     fn draw(&self, ctx: &mut Context, mut param: DrawParam) -> GameResult {
         let rect = Rect::new(0f32, 0f32, 1.0, 1.0);
 
-        param.rotation += self.body[0].ori;
-        param.scale.x *= self.body[0].size[0];
-        param.scale.y *= self.body[0].size[1];
-        param.dest.x += self.body[0].pos[0];
-        param.dest.y += self.body[0].pos[1];
+        let body = &self.body;
+        param.rotation += body.ori;
+        param.scale.x *= body.size[0];
+        param.scale.y *= body.size[1];
+        param.dest.x += body.pos[0];
+        param.dest.y += body.pos[1];
 
         let mesh = Mesh::new_rectangle(ctx, DrawMode::fill(), rect, graphics::WHITE)?;
         graphics::draw(ctx, &mesh, param)
